cbce6f438aa6ccab44d9cab8c06f71633958310b
Althaf <althafahammedm626@gmail.com>
2024-06-29 11:24:23 +0530

    exercise 1.3

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -9,12 +9,18 @@ interface PartProps {
 }
 
 interface ContentProps {
-  part1: string;
-  exercises1: number;
-  part2: string;
-  exercises2: number;
-  part3: string;
-  exercises3: number;
+  part1: {
+    name: string;
+    exercises: number;
+}
+  part2: {
+    name: string;
+    exercises: number;
+}
+  part3: {
+    name: string;
+    exercises: number;
+}
 }
 
 interface totalProps {
@@ -36,9 +42,9 @@ const Part: React.FC<PartProps> = ({ part, exercise }) => {
 const Content: React.FC<ContentProps> = (props) => {
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.part1.name} exercise={props.part1.exercises} />
+      <Part part={props.part2.name} exercise={props.part2.exercises} />
+      <Part part={props.part3.name} exercise={props.part3.exercises} />
     </>
   );
 };
@@ -49,24 +55,18 @@ const Total: React.FC<totalProps> = ({ total }) => {
 
 const App: React.FC = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const total = exercises1 + exercises2 + exercises3;
+  const part1 = {name: "Fundamentals of React", exercises: 10 }
+  const part2 = {name: "Using props to pass data", exercises: 7 }
+  const part3 = {name: "State of a component", exercises: 14 }
+  const total = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
       <Header course={course} />
       <Content
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
       <Total total={total} />
     </div>
