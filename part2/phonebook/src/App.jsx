9d060a152a011ae9900b35c014d88c9d03ec58ae
Althaf <althafahammedm626@gmail.com>
2024-07-13 11:57:54 +0530

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import AddPeople from "./AddPeople";
 import ShowPeople from "./ShowPeople";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setNewFilter] = useState("");
   const [personsToShow, setPersonsToShow] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons( response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setPersonsToShow(
+      persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    );
+  }, [persons, filter]);
 
   const handleChange = (event) => {
     setNewName(event.target.value);
